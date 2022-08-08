8b543c895a5ecece72d8cde59a4f609259e7a193
Raghunath Biswal <raghunath.biswal@ibm.com>
2022-08-08 16:15:59 +0530

    Adding cros origin fix

--- src/main/java/com/pru/test/onboarding/task/OnboardingTaskApp.java
+++ src/main/java/com/pru/test/onboarding/task/OnboardingTaskApp.java
@@ -2,12 +2,27 @@ package com.pru.test.onboarding.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OnboardingTaskApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnboardingTaskApp.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET","POST","PUT", "DELETE");
+			}
+		};
 	}
 
 }
--- src/main/java/com/pru/test/onboarding/task/controller/OnBoardingChecklistController.java
+++ src/main/java/com/pru/test/onboarding/task/controller/OnBoardingChecklistController.java
@@ -3,6 +3,7 @@ package com.pru.test.onboarding.task.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pru.test.onboarding.task.entity.OnBoardingChecklist;
 import com.pru.test.onboarding.task.service.OnBoardingChecklistService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/onboarding_checklist")
 public class OnBoardingChecklistController {
