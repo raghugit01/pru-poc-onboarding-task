af83658908bd971815acc51c6fe8447aad18cd5e
raghugit01 <biswalraghunath4@gmail.com>
2022-09-29 11:00:31 +0530

    cors issue fixed
    Adding code related to cors issue

--- src/main/java/com/pru/test/onboarding/task/ApplicationSecurity.java
+++ src/main/java/com/pru/test/onboarding/task/ApplicationSecurity.java
@@ -1,9 +1,12 @@
 package com.pru.test.onboarding.task;
 
+import java.util.Arrays;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,10 +15,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.pru.test.onboarding.task.jwt.JwtTokenFilter;
 
 @EnableWebSecurity(debug = true)
+@Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
@@ -33,8 +40,7 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		http.authorizeRequests().antMatchers("/token/get").permitAll().anyRequest().authenticated();
 		
@@ -56,4 +62,16 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- src/main/java/com/pru/test/onboarding/task/controller/OnBoardingChecklistController.java
+++ src/main/java/com/pru/test/onboarding/task/controller/OnBoardingChecklistController.java
@@ -2,11 +2,8 @@ package com.pru.test.onboarding.task.controller;
 
 import java.util.List;
 
-import javax.annotation.security.RolesAllowed;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pru.test.onboarding.task.entity.OnBoardingChecklist;
 import com.pru.test.onboarding.task.service.OnBoardingChecklistService;
 
-@CrossOrigin
+
 @RestController
 @RequestMapping("/onboarding_checklist")
 public class OnBoardingChecklistController {
